53638ae624796667e66e844be74b7f9f9686fc24
simondriesen <simon.driesen@hotmail.com>
2025-07-15 15:06:14 +0200

    #32 Add a type definition file

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+import { Map, MapMouseEvent, GeoJSONFeature } from 'maplibre-gl';
+
+declare module '@nazka/map-gl-js-spiderfy' {
+  export interface SpiderfyOptions {
+    minZoomLevel?: number;
+    zoomIncrement?: number;
+    circleSpiralSwitchover?: number;
+    circleFootSeparation?: number;
+    spiralFootSeparation?: number;
+    spiralLengthStart?: number;
+    spiralLengthFactor?: number;
+    animate?: boolean;
+    animationSpeed?: number;
+    legWeight?: number;
+    legColor?: string;
+    onLeafClick?: (feature: GeoJSONFeature, event: MapMouseEvent) => void;
+  }
+
+  export default class Spiderfy {
+    constructor(map: Map, options?: SpiderfyOptions);
+    applyTo(layerId: string): void;
+    unspiderfy(): void;
+  }
+}
