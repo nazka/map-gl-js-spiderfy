5e48dfe2e21fc660475ebbda8d4313099b52697b
simondriesen <simon.driesen@hotmail.com>
2025-09-03 12:18:20 +0200

    Wrong Typescript signature

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,23 +1,37 @@
-import type { Map, MapMouseEvent, GeoJSONFeature } from 'maplibre-gl';
+import type { Map, MapMouseEvent, GeoJSONFeature, SymbolLayerSpecification } from 'maplibre-gl';
 
 declare module '@nazka/map-gl-js-spiderfy' {
   export interface SpiderfyOptions {
+    onLeafClick?: (feature: GeoJSONFeature, event: MapMouseEvent) => void;
+    onLeafHover?: (feature: GeoJSONFeature, event: MapMouseEvent) => void;
     minZoomLevel?: number;
     zoomIncrement?: number;
+    closeOnLeafClick?: boolean;
     circleSpiralSwitchover?: number;
-    circleFootSeparation?: number;
-    spiralFootSeparation?: number;
-    spiralLengthStart?: number;
-    spiralLengthFactor?: number;
-    animate?: boolean;
-    animationSpeed?: number;
-    legWeight?: number;
-    legColor?: string;
-    onLeafClick?: (feature: GeoJSONFeature, event: MapMouseEvent) => void;
+    circleOptions?: {
+      leavesSeparation?: number;
+      leavesOffset?: [number, number];
+    }
+    spiralOptions?: {
+      legLengthStart?: number;
+      legLengthFactor?: number;
+      leavesSeparation?: number;
+      leavesOffset?: [number, number];
+    };
+    spiderLegsAreHidden?: boolean;
+    spiderLegsWidth?: number;
+    spiderLegsColor?: string;
+    spiderLeavesLayout?: SymbolLayerSpecification['layout'];
+    spiderLeavesPaint?: SymbolLayerSpecification['paint'];
+    maxLeaves?: number;
+    renderMethod?: 'flat' | '3D';
   }
 
   export default class Spiderfy {
-    constructor(map: Map, options?: SpiderfyOptions);
+    constructor(
+      map: Map, 
+      options?: SpiderfyOptions,
+    );
     applyTo(layerId: string): void;
     unspiderfyAll(): void;
   }
